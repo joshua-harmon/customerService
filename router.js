9d21964ab41c03268b5850a1258fb60fc3105196
Joshua-Harmon <joshua.harmon1960@gmail.com>
2022-06-29 15:19:16 -0700

    test

---
+++ router.js
@@ -0,0 +1,49 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
+
+Vue.use(Router);
+
+const router = new Router({
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      name: "Home",
+      component: Home
+    },
+    {
+      path: "/about",
+      name: "About",
+      component: () =>
+          import(/* webpackChunkName: "About" */ "./views/About"),
+    },
+    {
+      path: "/customerService",
+      name: "CustomerService",
+      component: () =>
+          import(/* webpackChunkName: "CustomerService" */ "./views/CustomerService"),
+    },
+    {
+      path: "/contact",
+      name: "Contact",
+      component: () =>
+          import(/* webpackChunkName: "Contact" */ "./views/Contact"),
+    },
+    {
+      path: "/success",
+      name: "Success",
+      component: () =>
+          import(/* webpackChunkName: "Success" */ "./views/Success"),
+    },
+    {
+      path: "/404",
+      alias: "*",
+      name: "NotFound",
+      component: () =>
+          import(/* webpackChunkName: "NotFound" */ "./views/NotFound"),
+    }
+  ]
+});
+
+export default router;
