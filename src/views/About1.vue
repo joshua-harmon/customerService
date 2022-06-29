e1257355e881b6206f986d4cb50dc040940853b6
Joshua-Harmon <joshua.harmon1960@gmail.com>
2022-06-29 16:44:02 -0700

    test

--- src/views/About1.vue
+++ src/views/About1.vue
@@ -2,79 +2,65 @@
   <section id="about1">
   <div class="box">
   <ul id="bookmarks">
-  <li><a href="#">support</a>
+  <li><a href="#">linux</a>
    <ul class="sites">
-    <li><a href="https://outsourcely.com/">oscely</a></li>
-    <li><a href="https://cloudpeeps.com">cloud</a></li>
-    <li><a href="https://workingnomads.com">nomads</a></li>
-    <li><a href="https://workew.com">workew</a></li>
-    <li><a href="https://nexrep.com">nexrep</a></li>
-    <li><a href="https://wrrk.com">wrrk</a></li>
-    <li><a href="https://search.yahoo.com/search?fr=mcafee&type=E211US1289G0&p=call+center">callCenter</a></li>
+    <li><a href="https://www.youtube.com/watch?v=HbgzrKJvDRw">fileSystem</a></li>  
+    <li><a href="https://www.youtube.com/watch?v=wBp0Rb-ZJak">linux</a></li>
+    <li><a href="https://www.youtube.com/watch?v=-ndFLv_hEn4">linux</a></li>
+    <li><a href="http://jeelabs.org/?s=ook">eLinux</a></li>
+    <li><a href="https://www.youtube.com/watch?v=uXD-Nuguhzc">redHat</a></li>
+    <li><a href="https://www.google.com/search?q=ubuntu+install+GenyMotion&oq=ubuntu+install+GenyMotion&aqs=chrome..69i57j0l5.7175j0j4&sourceid=chrome&ie=UTF-8">android</a></li>  
+    <li><a href="https://www.youtube.com/watch?v=UFIoRLqhFpo">fileSystm</a></li>  
+    <li><a href="https://www.youtube.com/watch?v=g8SzRhA8NXI">whonix</a></li>  
+    <li><a href="https://www.youtube.com/watch?v=2r2hVAe0zJ4">redHat</a></li>
+    <li><a href="https://www.youtube.com/watch?v=oSuy1TS8FGs">linuxRemte</a></li> 
+    <li><a href="https://www.youtube.com/watch?v=vZ7nI2im4Yg">linuxDistros</a></li>  
+    <li><a href="https://www.qubes-os.org/">qubesOs</a></li>
   </ul>
  </li>
-  <li><a href="#">remote</a>
+  <li><a href="#">ubuntu</a>
    <ul class="sites">
-    <li><a href="https://5ca.com/">5ca</a></li>
-    <li><a href="https://www.sitel.com/">sitel</a></li>
-    <li><a href="https://www.support.com/">support</a></li>
-    <li><a href="https://www.ttecjobs.com/">ttec</a></li>
-    <li><a href="https://jobs.workingsolutions.com/">ws</a></li>
-    <li><a href="https://www.fiverr.com/">fiverr</a></li>
-    <li><a href="https://upwork.com/">upwork</a></li>
-    <li><a href="https://idealist.org/">idealist</a></li>
-    <li><a href="https://remote4me.com/">remote</a></li>
+    <li><a href="http://sourceforge.net/projects/systemrescuecd/">rescueCd</a></li>
+    <li><a href="http://www.pendrivelinux.com/universal-usb-installer-easy-as-1-2-3/">usbOs</a></li>
+    <li><a href="https://www.youtube.com/watch?v=O1dN2YrOTvY">multiboot</a></li>
+    <li><a href="http://bileyg.com/news/1720">caeLinux</a></li>
+    <li><a href="https://askubuntu.com/questions/217555/how-to-use-verilog-hdl-on-ubuntu">ubntHlp</a></li>  
+    <li><a href="https://www.youtube.com/watch?v=oUVuiIOWp9c">ubuntu19.04</a></li>  
+    <li><a href="https://www.youtube.com/watch?v=vj45cOZRjm8">cinnamon</a></li>   
+    <li><a href="https://www.youtube.com/watch?v=IeABb8cwdUg">easyGimp</a></li>  
+    <li><a href="https://phoenixnap.com/kb/how-to-install-docker-on-ubuntu-18-04">ubuntuDocker</a></li>  
+    <li><a href="https://www.youtube.com/watch?v=4YywtDfnDI8">ubuntu19.10</a></li> 
+    <li><a href="https://www.youtube.com/watch?v=HMo9C7LCzE0">ubuntuOnRsby</a></li>  
   </ul>
  </li>
-  <li><a href="#">gigs</a>
+   <li><a href="#">usb</a>
    <ul class="sites">
-    <li><a href="https://affiliate-program.amazon.com">affiliate</a></li>
-    <li><a href="https://progressive.com">progressive</a></li>
-    <li><a href="https://magoosh.com">magoosh</a></li>
-    <li><a href="https://www.liveworld.com/careers/?job=1#jobs">liveworld</a></li>
-    <li><a href="https://onlinewritingjobs.com">writing</a></li>
-    <li><a href="https://writeraccess.com">writer</a></li>
-    <li><a href="https://peopleperhour.com">perhour</a></li>
-    <li><a href="https://percepta.com">percepta</a></li>
+    <li><a href="https://www.youtube.com/watch?v=UG9NH-T7jn4">ubuntuFlash</a></li>  
+    <li><a href="https://www.youtube.com/watch?v=ZVPYjh_yMqE">boot</a></li>  
+    <li><a href="https://www.youtube.com/watch?v=cIqVqfISvQo">bootableUbuntu</a></li>  
+    <li><a href="https://www.pendrivelinux.com/casper-rw-creator-make-a-persistent-file-from-windows/#more-3157">casperRw</a></li>  
+    <li><a href="https://www.youtube.com/watch?v=O9LU4jrDwoc">bootableUbuntu</a></li>  
+    <li><a href="https://www.youtube.com/watch?v=fLYBXOVn6ow">flashUbuntu</a></li>  
+    <li><a href="https://www.youtube.com/watch?v=UG9NH-T7jn4">ubuntuFlash</a></li>
+    <li><a href="https://etcher.io/">etcher</a></li>  
   </ul>
  </li>
-  <li><a href="#">mentors</a>
+  <li><a href="#">mint</a>
    <ul class="sites">
-    <li><a href="https://www.dreamhomebasedwork.com">mentor</a></li>
-    <li><a href="https://www.micromentor.org/">microMentor</a></li> 
-    <li><a href="https://www.youtube.com/watch?v=b5INK8IWfLE">bigmarkTV</a></li>
+    <li><a href="https://www.youtube.com/watch?v=yIfEKnxD3Nk">linuxMint</a></li>  
+    <li><a href="https://www.youtube.com/watch?v=MrOnPXqRggk">mint</a></li>  
+    <li><a href="https://www.youtube.com/watch?v=09hrTL5w-Jc">crsoLinx</a></li> 
+    <li><a href="https://cmints.io/">cmint</a></li>  
   </ul>
  </li>
-  <li><a href="#">freelancing</a>
+  <li><a href="#">more</a>
    <ul class="sites">
-    <li><a href="https://www.upwork.com/">upwork</a></li>
-    <li><a href="https://www.freelancer.com/">freelancer</a></li>
-    <li><a href="https://www.toptal.com/">toptal</a></li>
-    <li><a href="https://www.patreon.com/">patreon</a></li>
-    <li><a href="https://www.wwork.com/">wwork</a></li>
-    <li><a href="https://www.fiverr.com/">fiverr</a></li>
-    <li><a href="https://selly.io/">sellOnline</a></li> 
-  </ul>
- </li>
-  <li><a href="#">dropshipping</a>
-   <ul class="sites">
-    <li><a href="https://www.oberlo.com/?gclid=EAIaIQobChMIy43NvMzU3wIVexetBh2-xgqTEAEYASAAEgL52PD_BwE">Oberlo</a></li> 
-    <li><a href="https://sell.amazon.com/learn/what-is-dropshipping">Amazon</a></li>
-    <li><a href="https://dropshiplifestyle.com">videos</a></li>
-  </ul>
- </li>
-  <li><a href="#">typing</a>
-   <ul class="sites">
-    <li><a href="1888typeitupcom">typeItUp</a></li>
-    <li><a href="ratatype.com">ratatype</a></li>
-    <li><a href="typing.com">typing</a></li>
-  </ul>
- </li>
-  <li><a href="#">mre</a>
-   <ul class="sites">
-    <li><a href="tst1"></a></li>
-    <li><a href=""></a></li>
-    <li><a href=""></a></li>
+    <li><a href="https://www.networkworld.com/article/3269189/sharing-files-between-linux-and-windows.html">linxToWin</a></li>
+    <li><a href="https://www.youtube.com/watch?v=qHGTs1NSB1s">noUbuntu</a></li>  
+    <li><a href="https://www.youtube.com/watch?v=iEHr3B4KgTY">zorinOs</a></li>  
+    <li><a href="https://www.youtube.com/watch?v=mysM-V5h9z8">torvaldsInvw</a></li>  
+    <li><a href="https://www.amazon.com/dp/B07Q5DZJTK?tag=amz-mkt-chr-us-20&ascsubtag=1ba00-01000-org00-linux-other-nomod-us000-pcomp-feature-scomp-wm-5&ref=aa_scomp_srdg2">brainShrooms</a></li>  
+    <li><a href="https://www.swansonvitamins.com/eidon-ionic-minerals?DFA=1&UTM_Medium=PaidSearch&UTM_Source=GOOGLE&UTM_Campaign=SWAN_National_Gen_Search_NonBrnd_Phrase_National+Brand+Eidon+Ionic+Minerals&UTM_Content=ionic+minerals&SourceCode=INTLBVCNA&gclid=Cj0KCQiAr8bwBRD4ARIsAHa4YyIQRUP1Jqe4v17SJC1Cm37Un9eQRRnuxy2hpiSSZe-t4mABTvInWcIaAkpzEALw_wcB">supps</a></li>  
   </ul>
  </li>
 </ul>
